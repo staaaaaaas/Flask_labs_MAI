b6e7702f6194add835d744af980ab71812168cec
stas <46203376+staaaaaaas@users.noreply.github.com>
2022-06-07 23:54:46 +0300

    lab 3 ready

---
+++ lab3/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', url=url_for('show', message='this is app'))
+
+@app.route('/show/<string:message>')
+def show(message: str):
+    return message
+
+
+if __name__ == '__main__':
+    app.run()
